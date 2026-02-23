869a86194a35e2e3d12e2a31d8ef3c353747ef8f
Cahit Atilla Bozkurt <cbozkurt@k2m02s01.42kocaeli.com.tr>
2026-02-24 02:08:59 +0300

    swap

---
+++ swap.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/22 20:46:47 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/02/22 20:47:01 by cbozkurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <unistd.h>
+
+void    swap(t_node **stack)
+{
+    t_node  *first;
+    t_node  *second;
+
+    if (!stack || !*stack || !(*stack)->next)
+        return ;
+    first = *stack;
+    second = first->next;
+    first->next = second->next;
+    second->next = first;
+    *stack = second;
+}
+
+void	sa(t_node	**a)
+{
+	swap(a);
+	write(1, "sa\n", 3);
+}
+
+void	sb(t_node	**b)
+{
+	swap(b);
+	write(1, "sb\n", 3);
+}
+
+void	ss(t_node **a, t_node **b)
+{
+	swap(a);
+	swap(b);
+	write(1, "ss\n", 3);
+}
