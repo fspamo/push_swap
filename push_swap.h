1be4f621293cb1a991c66c04d95b2311699f618c
Cahit Atilla Bozkurt <cbozkurt@k1m06s01.42kocaeli.com.tr>
2026-02-26 13:18:31 +0300

    header file changed

--- push_swap.h
+++ push_swap.h
@@ -21,10 +21,23 @@ typedef struct	s_node
     struct s_node   *next;
 } t_node;
 
+int		default_controls(t_node *a);
+
 int		is_sorted(t_node *a);
 int		ft_atoi(const char *nptr);
-void	swap(t_node **stack);
-void    push(t_node **src, t_node **dest);
+
+void	sa(t_node	**a);
+void	sb(t_node	**a);
+void	ss(t_node	**a, t_node **b);
+
+void    pa(t_node **a, t_node **b);
+void    pb(t_node **b, t_node **a);
+
+
+void	ra(t_node **a);
+void	rb(t_node **b);
+void	rr(t_node **b, t_node **a);
+
 void	add_back(t_node **lst, t_node *next);
 
 #endif
