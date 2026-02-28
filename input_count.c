02a6355ccc101dfa531ea7e13de9be90705d3995
Cahit Atilla Bozkurt <cbozkurt@k2m16s01.42kocaeli.com.tr>
2026-03-01 01:59:11 +0300

    input counter added to track down data and send it to navigator

---
+++ input_count.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_count.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/28 20:46:24 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/02/28 22:49:08 by cbozkurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	input_counter(t_node *a)
+{
+	t_node	*holder;
+	int	count;
+
+	holder = a;
+	count = 0;
+	while (holder)
+	{
+		holder = holder->next;
+		count++;
+	}
+	return (count);
+}
