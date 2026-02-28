c02fbb0ad8e09c568d1a1878e579ac4bcb63603a
cbozkurt <cahitatillab@gmail.com>
2026-02-28 20:13:21 +0300

    added dupe control

--- controls.c
+++ controls.c
@@ -6,7 +6,7 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 00:26:32 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/02/25 00:42:45 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/02/28 19:27:49 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,26 @@
 void	default_controls(t_node *a)
 {
 	is_sorted(a);
+	is_duped(a);
+}
+
+int		is_duped(t_node *a)
+{
+	t_node	*curr;
+	t_node	*holder;
+
+	curr = a;
+	while (curr)
+	{
+		holder = curr->next;
+		while (holder)
+		{
+			if (holder->value == curr->value)
+				return (1);
+			holder = curr->next;
+		}
+	}
+	return (0);
 }
 
 int	is_sorted(t_node *a)
