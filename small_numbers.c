d8fe10f4330d456e77ff2f8ac7387832e63fdb03
Cahit Atilla Bozkurt <cbozkurt@k2m16s01.42kocaeli.com.tr>
2026-03-01 01:58:17 +0300

    five or less needs fix

--- small_numbers.c
+++ small_numbers.c
@@ -6,25 +6,97 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 01:04:42 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/02/28 02:24:05 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/03/01 00:58:21 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// do this
-
-void	two_numbers(t_node	*a)
+void	two_numbers(t_node	**a)
 {
-	
+	if ((*a) && (*a)->next && (*a)->value > (*a)->next->value)
+		sa(a);
 }
 
-void	three_numbers(t_node *a)
+void	three_numbers(t_node **a)
 {
+	int first = (*a)->value;
+	int second = (*a)->next->value;
+	int third = (*a)->next->next->value;
 
+	if (first > second && second < third && first < third)
+		sa(a);
+	else if (first > second && second > third)
+	{
+		sa(a);
+		rra(a);
+	}
+	else if (first > second && second < third && first > third)
+		ra(a);
+	else if (first < second && second > third && first < third)
+	{
+		sa(a);
+		ra(a);
+	}
+	else if (first < second && second > third && first > third)
+		rra(a);
 }
 
-void	five_or_less(t_node *a)
+int	find_min_five(t_node *a)
 {
+	t_node	*holder;
+	int		min;
+	int 	recent;
+	int 	i;
 
+	holder = a;
+	min = holder->value;
+	i = 0;
+	recent = 0;
+	while (holder)
+	{
+		if (holder->value < min)
+		{
+			min = holder->value;
+			recent = i;
+		}
+		holder = holder->next;
+		i++;
+	}
+	return (recent);
+}
+
+void	push_min_to_b(t_node **a, t_node **b)
+{
+	int	pos;
+	int	i;
+	int	size;
+
+	pos = find_min_five(*a);
+	size = input_counter(*a);
+
+	if (pos <= size / 2)
+	{
+		while (pos-- > 0)
+		{
+			ra(a);
+		}
+	}
+	else 
+	{
+		pos = size - pos;
+		while (pos-- > 0)
+			rra(a);
+	}
+	pb(a, b);
+}
+
+void	five_or_less(t_node **a, t_node **b)
+{
+	push_min_to_b(a, b);
+	push_min_to_b(a, b);
+
+	three_numbers(a);
+	pa(b, a);
+	pa(b, a);
 }
