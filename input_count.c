89f652f076eedad5cdee6e8d327d720effdaef01
Cahit Atilla Bozkurt <cbozkurt@k2m04s01.42kocaeli.com.tr>
2026-03-03 18:34:55 +0300

    commit

--- input_count.c
+++ input_count.c
@@ -12,6 +12,11 @@
 
 #include "push_swap.h"
 
+int	number_counter(int *array)
+{
+	
+}
+
 int	input_counter(t_node *a)
 {
 	t_node	*holder;
--- longest_increasing_never_ending_pain.c
+++ longest_increasing_never_ending_pain.c
@@ -11,11 +11,38 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdlib.h>
+
+int	*copy_stack(t_node *a, int size)
+{
+	int		*yanked_stack;
+	t_node	*instance;
+	size_t	i;
+
+	instance = a;
+	i = 0;
+	yanked_stack = malloc(size * sizeof(int));
+	if (!yanked_stack)
+		return (0);
+	while (instance && i < size)
+	{
+		yanked_stack[i] = instance->value;
+		instance = instance->next;
+		i++;
+	}
+	return (yanked_stack);
+}
+
+void	sort_array(int *array, int size)
+{
+	
+}
 
 void    index_stack(t_node *a)
 {
-	int	value_to_pass;
+	//  create a sorted array and pass arguments inside.
+	int	*array;
 
-
-
+	array = copy_stack(a, input_counter(a));
+	sort_array(array, );
 }
