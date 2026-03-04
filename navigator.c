fb3ba7391b58fbc057ce5d28f861311588ffcaf9
Cahit Atilla Bozkurt <cbozkurt@k2m02s01.42kocaeli.com.tr>
2026-03-05 01:40:23 +0300

    variable passing fixed

--- navigator.c
+++ navigator.c
@@ -12,20 +12,17 @@
 
 #include "push_swap.h"
 
-void	navigation(t_node	*a)
+void navigation(t_node **a)
 {
-	t_node	*b;
+    t_node *b;
 
-	(void)b;
-	if (input_counter(a) == 2)
-		two_numbers(&a);
-	else if (input_counter(a) == 3)
-		three_numbers(&a);
-	else if (input_counter(a) == 5 || input_counter(a) == 4)
-		five_or_less(&a, &b);
-	else if (input_counter(a) >= 6 && input_counter(a) <= 20)
-		bubble_sortingen(&a);
-	// else if (input_counter(a) == 10)
-	//  	core_keeper(&a, &b);
-
+    b = NULL;
+    if (input_counter(*a) == 2)
+        two_numbers(a);
+    else if (input_counter(*a) == 3)
+        three_numbers(a);
+    else if (input_counter(*a) == 5 || input_counter(*a) == 4)
+        five_or_less(a, &b);
+    else if (input_counter(*a) >= 6)
+        core_keeper(a, &b);
 }
