1eaca36a76ed938064192af8f86a19979c4b7b13
Cahit Atilla Bozkurt <cbozkurt@k2m16s01.42kocaeli.com.tr>
2026-03-06 07:56:46 +0300

    norm

--- hunting.c
+++ hunting.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-t_node *find_the_target(t_node *a, int target_val)
+t_node	*find_the_target(t_node *a, int target_val)
 {
 	t_node	*holder;
 	t_node	*best_pos;
@@ -22,10 +22,8 @@ t_node *find_the_target(t_node *a, int target_val)
 	while (holder)
 	{
 		if (holder->value > target_val)
-		{
 			if (best_pos == NULL || holder->value < best_pos->value)
 				best_pos = holder;
-		}
 		holder = holder->next;
 	}
 	if (best_pos == NULL)
@@ -42,55 +40,54 @@ t_node *find_the_target(t_node *a, int target_val)
 	return (best_pos);
 }
 
-int get_position(t_node *stack, t_node *target)
+int	get_position(t_node *stack, t_node *target)
 {
-    t_node *holder;
-    int pos;
+	t_node	*holder;
+	int		pos;
 
-    holder = stack;
-    pos = 0;
-    while (holder)
-    {
-        if (holder == target)
-            return (pos);
-        holder = holder->next;
-        pos++;
-    }
-    return (-1);
+	holder = stack;
+	pos = 0;
+	while (holder)
+	{
+		if (holder == target)
+			return (pos);
+		holder = holder->next;
+		pos++;
+	}
+	return (-1);
 }
 
-void rotate_to_target(t_node **a, t_node *target)
+void	rotate_to_target(t_node **a, t_node *target)
 {
-    int pos;
-    int size;
+	int	pos;
+	int	size;
 
-    pos = get_position(*a, target);
-    size = input_counter(*a);
-
-    if (pos <= size / 2)
-    {
-        while (*a != target)
-            ra(a);
-    }
-    else
-    {
-        while (*a != target)
-            rra(a);
-    }
+	pos = get_position(*a, target);
+	size = input_counter(*a);
+	if (pos <= size / 2)
+	{
+		while (*a != target)
+			ra(a);
+	}
+	else
+	{
+		while (*a != target)
+			rra(a);
+	}
 }
 
-void the_hunter(t_node **a, t_node **b)
+void	the_hunter(t_node **a, t_node **b)
 {
-    t_node *target;
-    int target_val;
+	t_node	*target;
+	int		target_val;
 
 	while (*b)
-    {
+	{
 		target_val = (*b)->value;
-
+		if (!target)
+			return;
 		target = find_the_target(*a, target_val);
-
 		rotate_to_target(a, target);
-		pa(a, b); 
+		pa(a, b);
 	}
 }
