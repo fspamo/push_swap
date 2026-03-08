6bbc7b645dc8708e9cfe1a1dd418b1bdf86decf4
cbozkurt <cahitatillab@gmail.com>
2026-03-08 14:00:24 +0300

    unecessary functions removed

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 13:39:30 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/03/05 03:05:22 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/03/08 00:59:30 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,6 @@
 typedef struct	s_node
 {
     int             value;
-    int             index;
-	int				lis_len;
-	int				in_lis;	
     struct s_node   *next;
 } t_node;
 
@@ -47,6 +44,8 @@ void		ra(t_node **a);
 void		rb(t_node **b);
 void		rr(t_node **b, t_node **a);
 void		rra(t_node **a);
+void		rrb(t_node **b);
+void		rrr(t_node **b, t_node **a);
         	
 void		add_back(t_node **lst, t_node *next);
 t_node		*new_lst(int value);
@@ -59,18 +58,7 @@ void		navigation(t_node **a);
 void		two_numbers(t_node **a);
 void		three_numbers(t_node **a);
 void		five_or_less(t_node **a, t_node **b);
-void		chungus(t_node **a, t_node **b);
 void		bubble_sortingen(t_node **a);
-
-void    	index_stack(t_node *a);
-int			calc_list_len(t_node *a);
-t_node		*last_lis_node(t_node *a, int max_len);
-void		mark_list(t_node *a, t_node *last);
-void		toss_to_b(t_node **a, t_node **b);
-void		core_keeper(t_node **a, t_node **b);
-void		the_hunter(t_node **a, t_node **b);
-void		rotate_to_target(t_node **a, t_node *target);
-void		final_rotate(t_node **a);
 
 // useless functions to clear up after debugging
 
