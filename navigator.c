017418785654c91d04b052dba42f6e7f5a01522a
cbozkurt <cahitatillab@gmail.com>
2026-03-08 18:17:02 +0300

    testing only the 3rd sorting algorithm

--- navigator.c
+++ navigator.c
@@ -6,7 +6,7 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 21:09:20 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/03/04 22:44:08 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/03/08 18:10:41 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ void navigation(t_node **a)
     t_node *b;
 
     b = NULL;
-    if (input_counter(*a) == 2)
-        two_numbers(a);
-    else if (input_counter(*a) == 3)
-        three_numbers(a);
-    else if (input_counter(*a) == 5 || input_counter(*a) == 4)
-        five_or_less(a, &b);
-    else if (input_counter(*a) >= 6)
-        core_keeper(a, &b);
+    // if (input_counter(*a) == 2)
+    //     two_numbers(a);
+    // else if (input_counter(*a) == 3)
+    //     three_numbers(a);
+    // else if (input_counter(*a) == 5 || input_counter(*a) == 4)
+    //     five_or_less(a, &b);
+    // else if (input_counter(*a) >= 6)
+    //     radix(a, &b);
+    if (input_counter(*a) >= 6)
+        radix(a, &b);
 }
