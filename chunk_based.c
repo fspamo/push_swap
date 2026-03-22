47999e9fa5175a8db6c518515a3aead2c24b6f6e
cahit <xahit1337@gmail.com>
2026-03-22 20:07:46 +0300

    chunk based created and in progress

--- chunk_based.c
+++ chunk_based.c
@@ -6,10 +6,17 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 14:44:20 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/03/22 14:45:30 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/03/22 19:59:39 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	chunk_sorting(t_node *a)
+{
+	int	size;
+	int sqrt;
 
+	size = input_counter(a);
+	sqrt = ft_sqrt(size);
+}
