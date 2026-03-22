2817f0604034ac3dc4e7e4c9ed23f8d3d31cda99
cahit <xahit1337@gmail.com>
2026-03-22 16:07:13 +0300

    indexer for chunk_based algorithm

---
+++ indexer_chunk.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   indexer_chunk.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/08 02:18:43 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/03/22 15:23:15 by cbozkurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <stdlib.h>
+
+int	*chunk_copy_array(t_node *a, int size)
+{
+	int		i;
+	int		*array;
+	t_node	*copycat;
+
+	i = 0;
+	copycat = a;
+	array = malloc(size * sizeof(int));
+	if (!array)
+		return (NULL);
+	while (copycat)
+	{
+		array[i] = copycat->value;
+		i++;
+		copycat = copycat->next;
+	}
+	return (array);
+}
+
+void	chunk_sort_arr(int *array, int size)
+{
+	int	temp;
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1 - i)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	chunk_index(t_node *a, int *array, int size)
+{
+	t_node	*holder;
+	int		i;
+
+	holder = a;
+	while (holder)
+	{
+		i = 0;
+		while (i < size)
+		{
+			if (array[i] == holder->value)
+			{
+				holder->index = i;
+				break;
+			}
+			i++;
+		}
+		holder = holder->next;
+	}
+}
+
+void	chunk_core_indexer(t_node *a)
+{
+	int *array;
+	int	size;
+
+	size = input_counter(a);
+	array = chunk_copy_array(a, size);
+	if (!array)
+		return ;
+	chunk_sort_arr(array, size);
+	chunk_index(a, array, size);
+	free(array);
+}
