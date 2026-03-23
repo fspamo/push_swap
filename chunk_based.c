8c34bec59d3734e996ff03ea9604ffd43504f829
Cahit Atilla Bozkurt <cbozkurt@k2m17s01.42kocaeli.com.tr>
2026-03-23 17:56:36 +0300

    half-way through the chunk_based

--- chunk_based.c
+++ chunk_based.c
@@ -11,12 +11,60 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+// #include <stdio.h>
 
-void	chunk_sorting(t_node *a)
+// void	print_stack_b(t_node *b)
+// {
+// 	printf("B stack:\n");
+// 	t_node	*to_print = b;
+// 	while (to_print)
+// 	{
+// 		printf("value: %d\n", to_print->value);
+// 		to_print = to_print->next;
+// 	}
+// }
+
+void	chunk_sorting(t_node **a, t_node **b)
 {
 	int	size;
-	int sqrt;
+	int	chunk;
+	int	i;
 
-	size = input_counter(a);
-	sqrt = ft_sqrt(size);
+	size = input_counter(*a);
+	chunk = ft_sqrt(size);
+	i = 0;
+	while (*a)
+	{
+		if ((*a)->index <= i)
+		{
+			pb(a, b);
+			rb(b);
+			i++;
+		}
+		else if ((*a)->index <= i + chunk)
+		{
+			pb(a, b);
+			i++;
+		}
+		else
+			ra(a);
+	}
+}
+
+int	find_max_index(t_node *b)
+{
+	t_node	*holder;
+	int		highest;
+
+	if (!b)
+		return (-1);
+	holder = b->next;
+	highest = b->index;
+	while (holder)
+	{
+		if (holder->index > highest)
+			highest = holder->index;
+		holder = holder->next;
+	}
+	return (highest); 
 }
