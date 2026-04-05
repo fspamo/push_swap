39ca2decdf8659664a08a5bce05da90dcc84e465
cahit <xahit1337@gmail.com>
2026-04-05 04:01:01 +0300

    ftprintft

--- benchingen.c
+++ benchingen.c
@@ -6,12 +6,12 @@
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 17:00:17 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/04/05 01:39:54 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/04/05 01:49:39 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include "ft_printf"
+#include "ft_printf/ft_printf.h"
 
 void    benchmarking(t_ops *ops, int strat, float disorder)
 {
