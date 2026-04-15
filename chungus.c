21433fd4f09f73627dfca15011297254363084bd
Cahit Atilla <xahit1337@gmail.com>
2026-04-16 00:48:04 +0300

    norm fixed

--- chungus.c
+++ chungus.c
@@ -1,28 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   chunk_based.c                                      :+:      :+:    :+:   */
+/*   chungus.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 14:44:20 by cbozkurt          #+#    #+#             */
-/*   Updated: 2026/03/22 19:59:39 by cbozkurt         ###   ########.fr       */
+/*   Updated: 2026/04/16 00:33:19 by cbozkurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-// #include <stdio.h>
-
-// void	print_stack_b(t_node *b)
-// {
-// 	printf("B stack:\n");
-// 	t_node	*to_print = b;
-// 	while (to_print)
-// 	{
-// 		printf("value: %d\n", to_print->value);
-// 		to_print = to_print->next;
-// 	}
-// }
 
 int	find_max_index(t_node *b)
 {
@@ -39,7 +27,7 @@ int	find_max_index(t_node *b)
 			highest = holder->index;
 		holder = holder->next;
 	}
-	return (highest); 
+	return (highest);
 }
 
 int	find_position(t_node *b, int highest)
@@ -59,60 +47,62 @@ int	find_position(t_node *b, int highest)
 	return (-1);
 }
 
-void chunker(t_node **a, t_node **b, t_ops *ops)
+void	chunker(t_node **a, t_node **b, t_ops *ops)
 {
-    int size;
-    int chunk;
-    int i;
-    size = input_counter(*a);
-    chunk = ft_sqrt(size);
-    i = 0;
-    while (*a)
-    {
-        if ((*a)->index <= i)
-        {
-            pb(a, b, ops);
-            rb(b, ops);
-            i++;
-        }
-        else if ((*a)->index <= i + chunk)
-        {
-            pb(a, b, ops);
-            i++;
-        }
-        else
-            ra(a, ops);
-    }
+	int	size;
+	int	chunk;
+	int	i;
+
+	size = input_counter(*a);
+	chunk = ft_sqrt(size);
+	i = 0;
+	while (*a)
+	{
+		if ((*a)->index <= i)
+		{
+			pb(a, b, ops);
+			rb(b, ops);
+			i++;
+		}
+		else if ((*a)->index <= i + chunk)
+		{
+			pb(a, b, ops);
+			i++;
+		}
+		else
+			ra(a, ops);
+	}
 }
 
-void chunk_sorting(t_node **a, t_node **b, t_ops *ops)
+static void	move_max_to_top(t_node **b, t_ops *ops)
 {
-    int max;
-    int pos;
-    int size;
-    int steps;
-    chunk_core_indexer(*a);
-    chunker(a, b, ops);
-    while (*b)
-    {
-        max = find_max_index(*b);
-        pos = find_position(*b, max);
-        size = input_counter(*b);
-        if (pos <= size / 2)
-            while (pos > 0)
-            {
-                rb(b, ops);
-                pos--;
-            }
-        else
-        {
-            steps = size - pos;
-            while (steps > 0)
-            {
-                rrb(b, ops);
-                steps--;
-            }
-        }
-        pa(a, b, ops);
-    }
+	int	max;
+	int	pos;
+	int	size;
+	int	steps;
+
+	max = find_max_index(*b);
+	pos = find_position(*b, max);
+	size = input_counter(*b);
+	steps = size - pos;
+	if (pos <= size / 2)
+		while (pos-- > 0)
+			rb(b, ops);
+	else
+	{
+		steps = size - pos;
+		while (steps-- > 0)
+			rrb(b, ops);
+	}
+}
+
+void	chunk_sorting(t_node **a, t_node **b, t_ops *ops)
+{
+	chunk_core_indexer(*a);
+	chunker(a, b, ops);
+	while (*b)
+	{
+		move_max_to_top(b, ops);
+		pa(a, b, ops);
+	}
 }
