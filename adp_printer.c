9d128b194bedbf01f3b3c8fb9c2e71407b6eef14
Cahit Atilla <xahit1337@gmail.com>
2026-04-20 13:05:53 +0300

    adp printer created added to makefile and header

---
+++ adp_printer.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   adp_printer.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbozkurt <cbozkurt@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 13:02:51 by cbozkurt          #+#    #+#             */
+/*   Updated: 2026/04/20 13:03:53 by cbozkurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+char	*adp_printer(float disorder)
+{
+	char	*metric;
+
+	if (disorder < 0.2)
+		metric = ft_strdup("Adaptive / O(n²)");
+	else if (0.2 <= disorder && disorder < 0.5)
+		metric = ft_strdup("Adaptive / O(n√n)");
+	else if (disorder >= 0.5)
+		metric = ft_strdup("Adaptive / O(n log n)");
+	return (metric);
+}
